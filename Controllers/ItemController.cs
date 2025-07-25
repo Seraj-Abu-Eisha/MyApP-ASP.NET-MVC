8c37c9f00348e9d8d752f4fd3cb5ad5fdf5dbf97
seraj <serajabueisha@gmail.com>
2025-07-25 23:16:59 +0300

    Initial commit

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyApP.Data;
+using MyApP.Models;
+
+namespace MyApP.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly MyAppContext _context;
+
+        public ItemController(MyAppContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var item = await _context.Items.ToListAsync();
+            return View(item);
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price")] Item item)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+    }
+}
---
+++ Data/MyAppContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApP.Models;
+namespace MyApP.Data
+{
+    public class MyAppContext : DbContext
+    {
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
+       
+         public DbSet<Item> Items { get; set; }
+    }
+}
